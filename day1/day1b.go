a86d0d404a6a803f781237921fabd8b4e8d26527
Thomas Meire <thomas.meire@acquia.com>
2015-12-01 20:36:55 +0100

    Add solution for day 1 puzzle, part b

---
+++ day1/day1b.go
@@ -0,0 +1,23 @@
+package main
+
+import "io/ioutil"
+
+func main() {
+	bs, err := ioutil.ReadFile("./input1.txt")
+	if err != nil {
+		panic("No such file")
+	}
+	floor := 0
+	for idx, b := range bs {
+		switch b {
+		case 40: // (
+			floor += 1
+		case 41: // )
+			floor -= 1
+		}
+		if floor == -1 {
+			print("Instruction: ", idx+1)
+			break
+		}
+	}
+}
