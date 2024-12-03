deb08e13171835ffec285b650077fb4a2e6e3b74
Thomas Meire <thomas@digitalpowertools.com>
2024-12-03 11:18:29 +0100

    Add an operation regex

--- 2024/day03/day03a.go
+++ 2024/day03/day03a.go
@@ -16,6 +16,7 @@ func atoi(s string) int {
 
 var mul = regexp.MustCompile(`mul\(([0-9]+),([0-9]+)\)`)
 var all = regexp.MustCompile(`(do)\(\)|(don't)\(\)|(mul)\(([0-9]+),([0-9]+)\)`)
+var ops = regexp.MustCompile(`(do|don't|mul)\((([0-9]+),([0-9]+))?\)`)
 
 func partA(prog string) int {
 	muls := mul.FindAllStringSubmatch(string(prog), -1)
@@ -51,8 +52,30 @@ func partB(prog string) int {
 	return sum
 }
 
+func partOps(prog string) int {
+	ins := ops.FindAllStringSubmatch(string(prog), -1)
+
+	sum := 0
+	enabled := true
+	for _, in := range ins {
+		switch in[1] {
+		case "do":
+			enabled = true
+		case "don't":
+			enabled = false
+		case "mul":
+			if enabled {
+				a := atoi(in[3])
+				b := atoi(in[4])
+				sum += a * b
+			}
+		}
+	}
+	return sum
+}
+
 func Solve() {
-	prog, err := os.ReadFile("./2024/day03/input.txt")
+	prog, err := os.ReadFile("./2024/day03/example2.txt")
 	if err != nil {
 		panic(err)
 	}
@@ -63,4 +86,6 @@ func Solve() {
 	sum = partB(string(prog))
 	println(sum)
 
+	sum = partOps(string(prog))
+	println(sum)
 }
